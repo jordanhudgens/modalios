a41c8634503812221cc992a37d55e07fa4adea16
Jordan Hudgens <jordan@wow.com>
2014-01-09 19:14:44 -0600

    Initial Commit

---
+++ Modal Demo/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  Modal Demo
+//
+//  Created by Jordan Hudgens on 1/9/14.
+//  Copyright (c) 2014 Jordan Hudgens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
